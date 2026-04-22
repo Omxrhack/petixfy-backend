3740477fc89ed3983fae68a3cde10c05f50d7976
omxrhack <bermejoosuna@gmail.com>
2026-04-21 22:23:04 -0700

    Add new environment variables and expand API routes in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ const petsRoutes = require('./pets.routes');
 const appointmentsRoutes = require('./appointments.routes');
 const emergenciesRoutes = require('./emergencies.routes');
 const productsRoutes = require('./products.routes');
+const rewardsRoutes = require('./rewards.routes');
+const triageRoutes = require('./triage.routes');
+const trackingRoutes = require('./tracking.routes');
 
 const router = Router();
 
@@ -11,5 +14,8 @@ router.use('/pets', requireAuth, petsRoutes);
 router.use('/appointments', requireAuth, appointmentsRoutes);
 router.use('/emergencies', requireAuth, emergenciesRoutes);
 router.use('/products', productsRoutes);
+router.use('/rewards', requireAuth, rewardsRoutes);
+router.use('/triage', requireAuth, triageRoutes);
+router.use('/tracking', requireAuth, trackingRoutes);
 
 module.exports = router;
